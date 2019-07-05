c88f7d160fb9ff290ee34ca9b5de2eefc49817c0
Trung <nguyenvantrung2015@gmail.com>
2019-07-05 10:03:19 +0700

    Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: %i(index edit update destroy)
+  before_action :correct_user, only: %i(edit update)
+  before_action :admin_user, only: :destroy
+  before_action :find_user, only: %i(show edit update destroy)
+
+  def index
+    @users = User.all.page(params[:page]).per(Settings.per_page_user)
+  end
+
   def new
     @user = User.new
   end
@@ -11,15 +20,34 @@ class UsersController < ApplicationController
       flash[:success] = t "user.alert_up"
       redirect_to @user
     else
-      flash[:success] = t "user.alert_up_fail"
+      flash[:danger] = t "user.alert_up_fail"
       render :new
     end
   end
 
   def show
-    @user = User.find_by id: params[:id]
     return if @user
     redirect_to signup_path
+  end
+
+  def edit; end
+
+  def update
+    if @user.update_attributes user_params
+      flash[:success] = t "user.updated.title_profile"
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @user.destroy
+      flash[:success] = t "user.delete.user-delete"
+    else
+      flash[:danger] = t "user.delete.user-no-delete"
+    end
+    redirect_to users_url
   end
 
   private
@@ -27,5 +55,25 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit :name, :email, :password,
       :password_confirmation
+  end
+
+  def logged_in_user
+    return if logged_in?
+    store_location
+    flash[:danger] = t "user.login.please-login"
+    redirect_to login_url
+  end
+
+  def correct_user
+    @user = User.find params[:id]
+    redirect_to root_url unless @user == current_user
+  end
+
+  def admin_user
+    redirect_to root_url unless current_user.admin?
+  end
+
+  def find_user
+    @user = User.find_by id: params[:id]
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,8 @@ class User < ApplicationRecord
 
   has_secure_password
   validates :password, presence: true,
-            length: {minimum: Settings.user.pass_min_length}
+            length: {minimum: Settings.user.pass_min_length},
+            allow_nil: true
 
   def self.digest string
     cost =  if ActiveModel::SecurePassword.min_cost
